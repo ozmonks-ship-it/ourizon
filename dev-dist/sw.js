beab8f188020fcb74c1746a73064194c0bbea647
Oz Monkul <ozgurmonkul@gmail.com>
2026-06-13 21:17:14 +1000

    Refactor bucket management: update createBucket function to improve parent-child relationships, enhance useLog hook for sub-bucket handling, and modify BucketsScreen for better expense management and sub-bucket addition.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.lt0vtqjgmrg"
+    "revision": "0.27tdi1bunuk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
